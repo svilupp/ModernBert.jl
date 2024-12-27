fe893e2530d0138edd30a532f19fd28b34de6087
J S <49557684+svilupp@users.noreply.github.com>
2024-12-27 10:06:22 -0600

    up

--- test/setup.jl
+++ test/setup.jl
@@ -2,4 +2,4 @@ using ModernBert: download_config_files, download_model
 
 download_model(REPO_URL, dirname(MODEL_PATH), "model_int8.onnx");
 ## Rename the model file to model.onnx
-mv(joinpath(dirname(MODEL_PATH), "model_int8.onnx"), MODEL_PATH)
+mv(joinpath(dirname(MODEL_PATH), "model_int8.onnx"), MODEL_PATH; force = true)
